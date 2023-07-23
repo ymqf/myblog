8f0946f65d14be15f0aa824d5f7dda2f20c85328
倚梦清风 <78645176+ymqf@users.noreply.github.com>
2023-07-23 22:56:11 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'sql.freedb.tech',
-  'user' => 'freedb_woshi120',
-  'password' => '?S&twJ!R$?Ne3bE',
+  'host' => 'containers-us-west-94.railway.app',
+  'user' => 'root',
+  'password' => 'fvSyhQQIOo1wtm0bn8D2',
   'charset' => 'utf8mb4',
-  'port' => '3306',
-  'database' => 'freedb_WOSHI120',
+  'port' => '7669',
+  'database' => 'railway',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
